30d64c43c42b40b72bb00b028770ca4f1f8343db
Irene <icabezasgomez@gmail.com>
2017-10-18 16:52:06 +0200

    acabada la prova

--- app/src/main/java/com/example/thor/provasettings/MainActivity.java
+++ app/src/main/java/com/example/thor/provasettings/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.thor.provasettings;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText.getText().toString();
+                textView.setText(name);
+
+            }
+        });
     }
+
+    @Override
+    protected void onPause() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor ed = prefs.edit();
+        ed.putString("name", textView.getText().toString());
+        ed.commit();
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onResume() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        editText.setText(prefs.getString("name",""));
+        textView.setText(prefs.getString("name", ""));
+        super.onResume();
+    }
+
+
 }
